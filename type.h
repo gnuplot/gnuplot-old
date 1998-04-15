4c648929999f71073fbdb00f281d87bedb628e66
lhecking <lhecking>
1998-04-15 19:16:25 +0000

    Initial revision

---
+++ type.h
@@ -0,0 +1,33 @@
+/* $Id: type.h,v 1.1 1998/04/15 19:16:35 lhecking Exp $ */
+
+/*
+ *	Header file: commonly used types
+ *
+ *
+ *	Copyright of this module:   Carsten Grammes, 1993
+ *      Experimental Physics, University of Saarbruecken, Germany
+ *
+ *      Internet address: ph12hucg@rz.uni-sb.de
+ *
+ *	Permission to use, copy, and distribute this software and its
+ *	documentation for any purpose with or without fee is hereby granted,
+ *	provided that the above copyright notice appear in all copies and
+ *	that both that copyright notice and this permission notice appear
+ *	in supporting documentation.
+ *
+ *      This software is provided "as is" without express or implied warranty.
+ */
+
+
+#ifndef TYPE_H
+#define TYPE_H
+
+#if !defined (FALSE) || !defined (TRUE)
+#define     FALSE   0
+#define     TRUE    1
+#endif
+
+typedef     char         boolean;
+typedef     unsigned short	word;
+
+#endif
