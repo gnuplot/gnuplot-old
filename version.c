31d93fa66b1d275824479a6c9365c690b49f0b60
lhecking <lhecking>
1998-09-23 19:59:25 +0000

    Bump version to 347pl3.

--- version.c
+++ version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: version.c,v 1.4 1998/09/21 21:00:11 lhecking Exp $";
+static char *RCSid = "$Id: version.c,v 1.5 1998/09/23 19:59:25 lhecking Exp $";
 #endif
 
 /* GNUPLOT - version.c */
@@ -46,8 +46,8 @@ static char *RCSid = "$Id: version.c,v 1.4 1998/09/21 21:00:11 lhecking Exp $";
  * do not agree with the one in the log. I hope to fix this some time soon.
  *
  * $Log: version.c,v $
- * Revision 1.4  1998/09/21 21:00:11  lhecking
- * Trim log entries.
+ * Revision 1.5  1998/09/23 19:59:25  lhecking
+ * Bump version to 347pl3.
  *
  * Revision 1.347  1998/06/22 12:24:56  ddenholm
  * dd : retrieve fit.*, matrix.*, docs/latextut/makefile from earlier vsns
@@ -1618,7 +1618,7 @@ static char *RCSid = "$Id: version.c,v 1.4 1998/09/21 21:00:11 lhecking Exp $";
 #include "plot.h"
 
 char version[] = "3.5 (pre 3.6)";
-char patchlevel[] = "beta 347pl2";
+char patchlevel[] = "beta 347pl3";
 char date[] = "Mon Jun 22 13:22:33 BST 1998"; 
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998";
 
