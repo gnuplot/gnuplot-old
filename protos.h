8ef92d09e537c639e3eb042e762ad25e20cb0415
lhecking <lhecking>
1998-11-17 20:54:52 +0000

    Update prototypes for help.c and term.c.

--- protos.h
+++ protos.h
@@ -1,5 +1,5 @@
 /*
- * $Id: protos.h,v 1.5 1998/11/06 22:09:45 lhecking Exp $
+ * $Id: protos.h,v 1.6 1998/11/17 20:54:52 lhecking Exp $
  *
  */
 
@@ -104,6 +104,12 @@ void write_multiline __PROTO((unsigned int x, unsigned int y, char *text_will_be
 void map3d_xy __PROTO((double x, double y, double z, unsigned int *xt, unsigned int *yt));
 int map3d_z __PROTO((double x, double y, double z));
 void do_3dplot __PROTO((struct surface_points *plots, int pcount));
+
+/* Prototypes from file "help.c" */
+
+int  help __PROTO((char *keyword, char *path, TBOOLEAN *subtopics));
+void FreeHelp __PROTO((void));
+void list_terms __PROTO((void));
 
 /* Prototypes from file "hidden3d.c" */
 
@@ -219,7 +225,7 @@ void term_reset __PROTO((void));
 void term_apply_lp_properties __PROTO((struct lp_style_type *lp));
 void term_check_multiplot_okay __PROTO((TBOOLEAN));
 
-void list_terms __PROTO((void));
+int  term_count __PROTO((void));
 struct termentry *set_term __PROTO((int));
 struct termentry *change_term __PROTO((char *name, int length));
 void init_terminal __PROTO((void));
