cc1e46e6f618b268f9a7cf5d61145461eeb14467
lhecking <lhecking>
1999-01-06 15:51:35 +0000

    Change date.

--- version.c
+++ version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: version.c,v 1.18 1998/12/22 19:42:47 lhecking Exp $";
+static char *RCSid = "$Id: version.c,v 1.19 1999/01/06 15:51:35 lhecking Exp $";
 #endif
 
 /* GNUPLOT - version.c */
@@ -39,7 +39,7 @@ static char *RCSid = "$Id: version.c,v 1.18 1998/12/22 19:42:47 lhecking Exp $";
 char version[] = "3.5 (pre 3.6)";
 /* char version[] = "3.7"; */
 char patchlevel[] = "beta 349";
-char date[] = "Tue Dec 22 19:41:30 BST 1998"; 
+char date[] = "Wed Jan  6 15:51:08 BST 1999"; 
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998";
 
 char faq_location[] = FAQ_LOCATION;
