6dafe041063a76d0cb4455d93632e4b1871dfb09
lhecking <lhecking>
1999-03-12 21:47:31 +0000

    Patchlevel 0.3.

--- version.c
+++ version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: version.c,v 1.25 1999/03/11 13:03:39 lhecking Exp $";
+static char *RCSid = "$Id: version.c,v 1.26 1999/03/12 21:47:31 lhecking Exp $";
 #endif
 
 /* GNUPLOT - version.c */
@@ -37,8 +37,8 @@ static char *RCSid = "$Id: version.c,v 1.25 1999/03/11 13:03:39 lhecking Exp $";
 #include "plot.h"
 
 char version[] = "3.7";
-char patchlevel[] = "0.2";
-char date[] = "Thu Mar 11 12:44:54 GMT 1999";
+char patchlevel[] = "0.3";
+char date[] = "Fri Mar 12 20:06:24 GMT 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1999";
 
 char faq_location[] = FAQ_LOCATION;
