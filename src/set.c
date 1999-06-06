c4d8acae85374c66d5dd2062179109416b1dc904
lhecking <lhecking>
1999-06-06 18:22:18 +0000

    Adjust gp_expand_tilde() call.

--- src/set.c
+++ src/set.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: set.c,v 1.9 1999/05/31 18:37:10 lhecking Exp $";
+static char *RCSid = "$Id: set.c,v 1.10 1999/06/06 18:22:18 lhecking Exp $";
 #endif
 
 /* GNUPLOT - set.c */
@@ -1181,7 +1181,7 @@ set_two()
 	    /* on int_error, we'd like to remember that this is allocated */
 	    static char *testfile = NULL;
 	    m_quote_capture(&testfile,c_token, c_token); /* reallocs store */
-	    gp_expand_tilde(&testfile,strlen(testfile));
+	    gp_expand_tilde(&testfile);
 	    /* Skip leading whitespace */
 	    while (isspace((int)*testfile))
 		testfile++;
