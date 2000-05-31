df857ac88225b75578f6c552f490d5e7b9428594
mikulik <mikulik>
2000-05-31 13:03:11 +0000

    Ignore `replot` command called from mouse/hotkey-event-enhanced terminal when some plot data were piped in gnuplot via stdin --- i.e., no mouse replot after `plot '-'`). See also "no replot after `plot '-'`" discussion in the gnuplot-beta newsgroup. New variables: plotted_data_from_stdin, replot_disabled.

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.18 2000/05/02 20:56:26 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.19 2000/05/31 13:03:11 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -631,6 +631,7 @@ int max_using;
 #endif /* PIPES */
 	/* I don't want to call strcmp(). Does it make a difference? */
     if (*filename == '-' && strlen(filename) == 1) {
+	plotted_data_from_stdin = 1;
 	data_fp = lf_top();
 	if (!data_fp)
 	    data_fp = stdin;
