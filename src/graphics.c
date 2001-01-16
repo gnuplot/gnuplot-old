dda7746f4b1379d871ffc35004bf12f48385bed0
broeker <broeker>
2001-01-16 20:21:59 +0000

    2 minor nitpicks fixed

--- src/graphics.c
+++ src/graphics.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: graphics.c,v 1.41 2000/12/05 19:03:02 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: graphics.c,v 1.42 2001/01/16 20:21:59 broeker Exp $"); }
 #endif
 
 /* GNUPLOT - graphics.c */
@@ -965,7 +965,7 @@ apply_head_properties(struct position* headsize)
 {
     curr_arrow_headlength = 0;
     if (headsize->x > 0) { /* set head length+angle for term->arrow */
-	int itmp, x1, x2;
+	unsigned int itmp, x1, x2;
 	double savex = headsize->x;
 
 	curr_arrow_headangle = headsize->y;
