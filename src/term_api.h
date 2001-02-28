ac219ae8ecb99a195869a826fdff4c44ea84d3ef
broeker <broeker>
2001-02-28 16:41:09 +0000

    Replaced {re}set_lp_properties() by intialization / struct assignment

--- src/term_api.h
+++ src/term_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: term_api.h,v 1.12 2000/12/18 08:21:17 mikulik Exp $
+ * $Id: term_api.h,v 1.13 2001/02/28 16:41:09 broeker Exp $
  */
 
 /* GNUPLOT - term_api.h */
@@ -75,6 +75,12 @@ typedef struct lp_style_type {	/* contains all Line and Point properties */
 #endif
     /* ... more to come ? */
 } lp_style_type;
+
+#ifdef PM3D
+# define DEFAULT_LP_STYLE_TYPE {0, 0, 0, 1.0, 1.0, FALSE}
+#else
+# define DEFAULT_LP_STYLE_TYPE {0, 0, 0, 1.0, 1.0}
+#endif
 
 /* values for the optional flags field - choose sensible defaults
  * these aren't really very sensible names - multiplot attributes
