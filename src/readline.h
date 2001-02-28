a620820bb39b28e82bf2cae9c52eb74837da9a2e
broeker <broeker>
2001-02-28 16:12:24 +0000

    readline_ipc() prototype moved to saner place

--- src/readline.h
+++ src/readline.h
@@ -1,5 +1,5 @@
 /*
- * $Id: readline.h,v 1.7 2000/11/01 18:57:33 broeker Exp $
+ * $Id: readline.h,v 1.8 2001/02/28 16:12:24 broeker Exp $
  */
 
 /* GNUPLOT - readline.h */
@@ -54,6 +54,12 @@
 #if defined(READLINE) && !defined(HAVE_LIBREADLINE)
 char *readline __PROTO((const char *));
 #endif
+
+/*
+ * The following 'readline_ipc' routine is usual 'readline' for OS2_IPC,
+ * and a special one for IPC communication.
+ */
+char *readline_ipc __PROTO((const char*));
 
 #if defined(ATARI) || defined(MTOS)
 char tos_getch __PROTO((void));
