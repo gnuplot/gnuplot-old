5c9e804501f83359bc76ec1f39fad55b04cd37ec
mikulik <mikulik>
2001-09-27 19:52:50 +0000

    new option 'fix' in 'set autoscale {{axis}{...|fix} | fix}' to easily set fixmin and fixmax in one command for one or all axes

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.22 2001/09/17 12:06:38 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.23 2001/09/27 19:52:50 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -571,8 +571,8 @@ unset_autoscale()
 	if (axis >= 0) {
 	    axis_array[axis].set_autoscale = AUTOSCALE_NONE;
 	c_token++;
+	}
     }
-}
 }
 
 
