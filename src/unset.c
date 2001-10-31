39afe55b597cef339718044eb1682c6795d5c92d
mikulik <mikulik>
2001-10-31 17:13:59 +0000

    New options for set grid: layerdefault, front, back. Further, labels and arrows were not saving back, front option.

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.23 2001/09/27 19:52:50 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.24 2001/10/31 17:13:59 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -1426,6 +1426,7 @@ reset_command()
     grid_lp = default_grid_lp;
     mgrid_lp = default_grid_lp;
     polar_grid_angle = 0;
+    grid_layer = -1;
 
     hidden3d = FALSE;
 
