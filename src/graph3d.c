4fb4abe7e1b122b803b31157447dd11596b51a33
amai <amai>
2002-02-15 17:06:31 +0000

    Add missing #include

--- src/graph3d.c
+++ src/graph3d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.59 2002/02/13 22:58:17 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.60 2002/02/15 17:06:31 amai Exp $"); }
 #endif
 
 /* GNUPLOT - graph3d.c */
@@ -65,6 +65,7 @@ static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.59 2002/02/13 22:58:17 m
 
 #ifdef PM3D
 #   include "pm3d.h"
+#   include "plot3d.h"
 #   include "color.h"
 #endif
 
