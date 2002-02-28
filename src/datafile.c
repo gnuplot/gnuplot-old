bc69dc26af2296fe2de31f61396755a1b5aaa7f0
mikulik <mikulik>
2002-02-28 09:36:43 +0000

    Moved color axis autoscaling and pm3d zminmax setting to axis.c in order to improve the source code modularity.

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.34 2002/02/27 21:18:54 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.35 2002/02/28 09:36:44 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -151,9 +151,6 @@ static char *RCSid() { return RCSid("$Id: datafile.c,v 1.34 2002/02/27 21:18:54 
 #include "misc.h"
 #include "parse.h"
 #include "plot.h"
-#ifdef PM3D
-# include "plot3d.h"
-#endif
 #include "util.h"
 
 /* if you change this, change the scanf in readline */
