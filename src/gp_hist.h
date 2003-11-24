34d09da73ea67d0ebd24d330bb9c1e46049f43d0
mikulik <mikulik>
2003-11-24 16:22:30 +0000

    Allow 'history "file"' be a pipe. New option 'history "file" append'.

--- src/gp_hist.h
+++ src/gp_hist.h
@@ -1,5 +1,5 @@
 /*
- * $Id: gp_hist.h,v 1.4 2000/11/01 18:57:28 broeker Exp $
+ * $Id: gp_hist.h,v 1.5 2003/11/24 16:22:31 mikulik Exp $
  */
 
 /* GNUPLOT - gp_hist.h */
@@ -64,7 +64,7 @@ extern struct hist *cur_entry;
 
 #if defined(READLINE) && !defined(HAVE_LIBREADLINE)
 void add_history __PROTO((char *line));
-void write_history_n __PROTO((const int, const char *));
+void write_history_n __PROTO((const int, const char *, const char *));
 void write_history __PROTO((char *));
 char *history_find __PROTO((char *));
 int history_find_all __PROTO((char *));
