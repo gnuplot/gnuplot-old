7a37cb979858989f73dcbd4f60d1922bd70b4e87
sfeam <sfeam>
2006-04-05 03:14:12 +0000

    Fix breakage due to accidental deletion of call to arrow_parse()

--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.124 2006/03/28 21:52:13 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.125 2006/04/05 03:14:12 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -1541,7 +1541,7 @@ eval_plots()
                         this_plot->arrow_properties.lp_properties.l_type = line_num;
                     }
 
-
+		    arrow_parse(&(this_plot->arrow_properties), TRUE);
                     if (stored_token != c_token) {
                         if (set_lpstyle) {
                             duplication=TRUE;
