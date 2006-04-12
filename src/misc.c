760b25e25e0652cb9b824857810fc1506ec35370
sfeam <sfeam>
2006-04-12 03:48:48 +0000

    Avoid a recursion trap in setting line styles after 'set style incr user'

--- src/misc.c
+++ src/misc.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: misc.c,v 1.79 2006/04/05 03:00:48 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: misc.c,v 1.80 2006/04/12 03:48:50 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - misc.c */
@@ -791,7 +791,7 @@ lp_parse(struct lp_style_type *lp, TBOOLEAN allow_ls, TBOOLEAN allow_point)
 		    int lt = real(const_express(&t));
 		    lp->l_type = lt - 1;
 		    /* user may prefer explicit line styles */
-		    if (prefer_line_styles)
+		    if (prefer_line_styles && allow_ls)
 			lp_use_properties(lp, lt, TRUE);
 		}
 	    } /* linetype, lt */
