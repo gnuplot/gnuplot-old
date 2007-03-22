bc19a067f89ba175800acf12a466ec017f44a4dd
sfeam <sfeam>
2007-03-22 04:25:40 +0000

    Reduce the problem of trailing comma after 2D offset

--- src/axis.c
+++ src/axis.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: axis.c,v 1.60.2.1 2006/10/22 12:09:43 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: axis.c,v 1.60.2.2 2007/03/22 04:25:41 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - axis.c */
@@ -1609,7 +1609,10 @@ get_position_default(struct position *pos, enum position_type default_type)
     }
 
     /* z is not really allowed for a screen co-ordinate, but keep it simple ! */
-    if (equals(c_token, ",")) {
+    if (equals(c_token, ",")
+       /* Partial fix for ambiguous syntax when trailing comma ends a plot command */
+	&& !(isstringvalue(c_token+1))
+       ) {
 	++c_token;
 	get_position_type(&type, &axes);
 	pos->scalez = type;
