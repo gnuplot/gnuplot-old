08ff64740fa5cfd2dcc6e303c7d1e1020968b858
sfeam <sfeam>
2007-08-31 20:03:39 +0000

    Allow zooming and refresh of volatile data [make Octave users happy]

--- src/set.c
+++ src/set.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: set.c,v 1.250 2007/08/27 04:33:48 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: set.c,v 1.251 2007/08/31 20:03:44 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - set.c */
@@ -2071,6 +2071,12 @@ set_logscale()
 		axis_array[axis].base = newbase;
 	}
     }
+
+#ifdef VOLATILE_REFRESH
+    /* Because the log scaling is applied during data input, a quick refresh */
+    /* using existing stored data will not work if the log setting changes.  */
+    refresh_ok = 0;
+#endif
 }
 
 #ifdef GP_MACROS
