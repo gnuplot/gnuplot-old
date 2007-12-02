c3ce4c93b6959fe4ae8d8939e3d15f6e14770ae4
sfeam <sfeam>
2007-12-02 04:21:46 +0000

    Warn on enhanced text mode syntax error

--- src/term.c
+++ src/term.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: term.c,v 1.166 2007/12/02 01:48:14 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: term.c,v 1.167 2007/12/02 04:21:48 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - term.c */
@@ -2461,6 +2461,9 @@ enhanced_recursion(
 
 			ENH_DEBUG(("Font size %.1f\n", f));
 			/*}}}*/
+		    } else if (ch == '}') {
+			int_warn(NO_CARET,"bad syntax in enhanced text string");
+			*p++ = '\0';
 		    } else {
 			*p++ = '\0';
 			f = fontsize;
