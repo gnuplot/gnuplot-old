196d808d9e0c6beeed193f410a94fbe50d785120
sfeam <sfeam>
2008-02-24 19:49:32 +0000

    Replace bare "unsigned" with "unsigned int" in order to facilitate type checking

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.139 2008/02/23 17:43:47 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.140 2008/02/24 19:49:35 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -3119,7 +3119,7 @@ adjust_binary_use_spec()
 
     char *nothing_known = "No default columns known for that plot style";
     enum PLOT_STYLE plot_style;
-    unsigned ps_index;
+    unsigned int ps_index;
     int c_token_copy;
 
     /* This may appear strange, but ASCII matrix is not the same
