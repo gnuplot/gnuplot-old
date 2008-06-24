4e0b1906c79e39ce5be8cb84c11e06a71b1c7057
sfeam <sfeam>
2008-06-24 18:46:15 +0000

    Don't forget to draw a polyline even if it has only a single segment

--- src/win/wgraph.c
+++ src/win/wgraph.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: wgraph.c,v 1.52.2.2 2008/06/18 16:49:46 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: wgraph.c,v 1.52.2.3 2008/06/24 18:46:15 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - win/wgraph.c */
@@ -866,6 +866,8 @@ drawgraph(LPGW lpgw, HDC hdc, LPRECT rect)
 	if ((lastop==W_vect) && (curptr->op!=W_vect)) {
 	    if (polyi >= 2)
 		Polyline(hdc, ppt, polyi);
+	    else if (polyi == 1)
+		LineTo(hdc, ppt, polyi);
 	    polyi = 0;
 	}
 	switch (curptr->op) {
