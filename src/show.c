7551f5469977b3e059c88e8d42e6e4f398b35598
sfeam <sfeam>
2008-11-07 06:34:21 +0000

    Allow tics to be set front/back without turning on the whole grid

--- src/show.c
+++ src/show.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: show.c,v 1.185.2.6 2008/09/23 23:11:34 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: show.c,v 1.185.2.7 2008/11/07 06:34:22 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - show.c */
@@ -2602,6 +2602,9 @@ show_tics(
     else
 	fprintf(stderr, "xyplane ticslevel is %g\n", xyplane.ticslevel);
 
+    if (grid_layer >= 0)
+        fprintf(stderr, "tics are in %s of plot\n", (grid_layer==0) ? "back" : "front");
+	
     if (showx)
 	show_ticdef(FIRST_X_AXIS);
     if (showx2)
