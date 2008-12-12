b54c7dbbf0f61bc8d46a68a45600dced7727501c
sfeam <sfeam>
2008-12-12 06:57:50 +0000

    Support use of NetBSD libedit as an alternative to gnu libreadline

--- src/readline.h
+++ src/readline.h
@@ -1,5 +1,5 @@
 /*
- * $Id: readline.h,v 1.9 2004/04/13 17:23:59 broeker Exp $
+ * $Id: readline.h,v 1.9.4.1 2008/12/12 06:57:50 sfeam Exp $
  */
 
 /* GNUPLOT - readline.h */
@@ -46,12 +46,19 @@
 
 /* Prototypes of functions exported by readline.c */
 
-#ifdef HAVE_LIBREADLINE
+#if defined(HAVE_LIBREADLINE)
 # include "stdfn.h"	/* <readline/readline.h> needs stdio.h... */
 # include <readline/readline.h>
 #endif
+#if defined(HAVE_LIBEDITLINE)
+# include <editline/readline.h>
+#endif
 
-#if defined(READLINE) && !defined(HAVE_LIBREADLINE)
+#if defined(HAVE_LIBEDITLINE)
+int getc_wrapper __PROTO((FILE* fp));
+#endif
+
+#if defined(READLINE) && !defined(HAVE_LIBREADLINE) && !defined(HAVE_LIBEDITLINE)
 char *readline __PROTO((const char *));
 #endif
 
