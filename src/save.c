f35ebbc517ebe176ec8e3a06874fcec129e1bcae
sfeam <sfeam>
2009-06-06 18:28:42 +0000

    Add new linestyle property: pointinterval

--- src/save.c
+++ src/save.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: save.c,v 1.171 2009/03/28 21:15:04 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: save.c,v 1.172 2009/06/06 18:28:43 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - save.c */
@@ -1264,6 +1264,7 @@ save_linetype(FILE *fp, lp_style_type *lp, TBOOLEAN show_point)
 	    fprintf(fp, " pointsize default");
 	else
 	    fprintf(fp, " pointsize %.3f", lp->p_size);
+	fprintf(fp, " pointinterval %d", lp->p_interval);
     }
 	
 }
