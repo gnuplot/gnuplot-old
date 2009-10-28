50b71c7626e80f1adf50be148877ff1b26f28319
sfeam <sfeam>
2009-10-28 06:46:26 +0000

    Repair missing brackets in if/else

--- src/save.c
+++ src/save.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: save.c,v 1.173 2009/07/16 15:47:22 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: save.c,v 1.174 2009/10/28 06:46:27 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - save.c */
@@ -1102,12 +1102,13 @@ save_fillstyle(FILE *fp, const struct fill_style_type *fs)
 	fprintf(fp, " empty ");
 	break;
     }
-    if (fs->border_color.type == TC_LT && fs->border_color.lt == LT_NODRAW)
+    if (fs->border_color.type == TC_LT && fs->border_color.lt == LT_NODRAW) {
 	fprintf(fp, "noborder\n");
-    else
+    } else {
 	fprintf(fp, "border");
 	save_pm3dcolor(fp, &fs->border_color);
 	fprintf(fp, "\n");
+    }
 }
 
 void
