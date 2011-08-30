3d02b5fd8e48fc0c00b060e0c6e0f619312c9234
sfeam <sfeam>
2011-08-30 04:11:04 +0000

    Make "assert" statements trigger int_error() rather than abort()

--- src/getcolor.c
+++ src/getcolor.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: getcolor.c,v 1.29 2011/06/21 01:10:43 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: getcolor.c,v 1.30 2011/08/30 04:11:04 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - getcolor.c */
@@ -477,7 +477,6 @@ approximate_palette(t_sm_palette *palette, int samples,
     gradient = (gradient_struct*)
 	malloc(gradient_size * sizeof(gradient_struct));
     colors = (rgb_color*) malloc(colors_size * sizeof(rgb_color));
-    assert(gradient && colors);
 
     /* start (gray=0.0) is needed */
     cnt = 0;
