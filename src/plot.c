997c7d7e20ca37aa6ee09755ddaf000ccba172cc
sfeam <sfeam>
2012-05-06 22:52:55 +0000

    avoid calling buggy libedit emulation of using_history()

--- src/plot.c
+++ src/plot.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot.c,v 1.128.2.10 2012/03/02 20:00:04 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot.c,v 1.128.2.11 2012/05/06 22:52:55 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot.c */
@@ -333,7 +333,11 @@ main(int argc, char **argv)
      * instead of readline) */
     rl_readline_name = "Gnuplot";
     rl_terminal_name = getenv("TERM");
+#if defined(HAVE_LIBREADLINE)
     using_history();
+#else
+    history_init();
+#endif
 #endif
 #if defined(HAVE_LIBREADLINE) && !defined(MISSING_RL_TILDE_EXPANSION)
     rl_complete_with_tilde_expansion = 1;
