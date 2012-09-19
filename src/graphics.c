9ddc6aeb4999fdc6694a676131f03d4da8bd9f26
sfeam <sfeam>
2012-09-19 00:22:06 +0000

    fix incorrect default layout of rotate xtic labels

--- src/graphics.c
+++ src/graphics.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: graphics.c,v 1.379.2.10 2012/09/11 22:42:37 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: graphics.c,v 1.379.2.11 2012/09/19 00:22:06 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - graphics.c */
@@ -1025,8 +1025,6 @@ boundary(struct curve_points *plots, int count)
 	    projection = -sin((double)axis_array[FIRST_X_AXIS].tic_rotate*DEG2RAD);
 	if (axis_array[FIRST_X_AXIS].label.pos == RIGHT)
 	    projection *= -1;
-	else if (axis_array[FIRST_X_AXIS].label.pos == CENTRE)
-	    projection = 0.5*fabs(projection);	
 	widest_tic_strlen = 0;		/* reset the global variable ... */
 	gen_tics(FIRST_X_AXIS, /* 0, */ widest_tic_callback);
 
