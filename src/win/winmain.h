f4f6f860350764fe67f9d6b513e77be0f81733a6
markisch <markisch>
2012-11-26 08:18:23 +0000

    Move and rename function to handle Windows messages.

--- src/win/winmain.h
+++ src/win/winmain.h
@@ -1,5 +1,5 @@
 /*
- * $Id: winmain.h,v 1.11 2012/10/08 22:30:53 markisch Exp $
+ * $Id: winmain.h,v 1.12 2012/11/26 08:18:23 markisch Exp $
  */
 
 /* GNUPLOT - win/winmain.h */
@@ -66,5 +66,6 @@ void kill_pending_Pause_dialog __PROTO((void));
 void win_sleep __PROTO((DWORD dwMilliSeconds));
 TBOOLEAN WinWindowOpened(void);
 void WinPersistTextClose(void);
+void WinMessageLoop(void);
 
 #endif /* GNUPLOT_WINMAIN_H */
