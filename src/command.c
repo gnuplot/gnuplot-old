3a9d1ec5f59faf37bd6d8ec1e4fb0753fbdab886
sfeam <sfeam>
2013-01-04 20:52:51 +0000

    conditional code for --disable-volatile-data

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: command.c,v 1.230.2.6 2012/10/30 19:13:18 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: command.c,v 1.230.2.7 2013/01/04 20:52:52 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - command.c */
@@ -2166,7 +2166,9 @@ replotrequest()
 	free(replot_args);
     }
     plot_token = 0;		/* whole line to be saved as replot line */
+#ifdef VOLATILE_REFRESH
     refresh_ok = 0;		/* start of replot will destory existing data */
+#endif
 
     screen_ok = FALSE;
     num_tokens = scanner(&gp_input_line, &gp_input_line_len);
