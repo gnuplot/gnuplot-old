f391de7475c34d1fc3114aa7763a6c7bae45a726
sfeam <sfeam>
2013-04-23 01:42:59 +0000

    Fix memory leak caused by  set key font "foo"; reset

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.164 2013/04/21 06:26:11 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.165 2013/04/23 01:42:59 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -862,6 +862,7 @@ void
 reset_key()
 {
     legend_key temp_key = DEFAULT_KEY_PROPS;
+    free(keyT.font);
     memcpy(&keyT, &temp_key, sizeof(keyT));
 }
 
