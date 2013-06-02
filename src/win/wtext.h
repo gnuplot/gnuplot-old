8f4fe04318095990cd551839c2225de682c0c2ee
markisch <markisch>
2013-06-02 13:59:26 +0000

    Redirect cout and cerr to text window

--- src/win/wtext.h
+++ src/win/wtext.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wtext.h,v 1.15 2013/03/02 16:07:43 broeker Exp $
+ * $Id: wtext.h,v 1.16 2013/06/02 13:59:26 markisch Exp $
  */
 
 /* GNUPLOT - win/wtext.h */
@@ -37,8 +37,13 @@
 #ifndef GNUPLOT_WTEXT_H
 #define GNUPLOT_WTEXT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stdarg.h>
+#include "syscfg.h"
 
 /* redefine functions that can talk to tty devices, to use
  * implementation in winmain.c */
@@ -117,6 +122,9 @@ FILE *fake_popen(const char *command, const char *type);
 int fake_pclose(FILE *stream);
 #endif
 
+/* redirect C++ standard output streams */
+void RedirectOutputStreams(int init);
+
 #else /* WGP_CONSOLE */
 
 #define getch ConsoleGetch
@@ -125,5 +133,9 @@ int fake_pclose(FILE *stream);
 int ConsoleGetch();
 
 #endif /* WGP_CONSOLE */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* GNUPLOT_WTEXT_H */
