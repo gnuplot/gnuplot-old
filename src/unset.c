bce6c3d5aa2afe43e0671be93edc78163ae29ded
sfeam <sfeam>
2013-09-11 23:38:35 +0000

    reset style increment

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.149.2.2 2012/10/09 16:30:15 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.149.2.3 2013/09/11 23:38:38 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -1744,6 +1744,7 @@ reset_command()
 #ifdef VOLATILE_REFRESH
     refresh_ok = 0;
 #endif
+    prefer_line_styles = FALSE;
 
     reset_hidden3doptions();
     hidden3d = FALSE;
