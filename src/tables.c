328929103d0b88e29d9d4fe54d87aaa703a94f94
sfeam <sfeam>
2013-12-27 02:56:04 +0000

    new smoothing option for piecewise monotonic cubic splines

--- src/tables.c
+++ src/tables.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: tables.c,v 1.110 2011/11/10 05:15:58 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: tables.c,v 1.110.2.1 2013/12/27 02:56:06 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - tables.c */
@@ -121,6 +121,7 @@ const struct gen_table plot_smooth_tbl[] =
     { "cum$ulative", SMOOTH_CUMULATIVE },
     { "k$density", SMOOTH_KDENSITY },
     { "cn$ormal", SMOOTH_CUMULATIVE_NORMALISED },
+    { "mcs$plines", SMOOTH_MONOTONE_CSPLINE },
     { NULL, SMOOTH_NONE }
 };
 
