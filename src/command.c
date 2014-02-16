22570e071ce5ea790800c2d645018a7cb2fd542a
sfeam <sfeam>
2014-02-16 21:34:58 +0000

    Fix conditional compilation with --disable-mouse

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: command.c,v 1.277 2014/01/29 15:48:51 lodewyck Exp $"); }
+static char *RCSid() { return RCSid("$Id: command.c,v 1.278 2014/02/16 21:34:58 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - command.c */
@@ -1349,7 +1349,7 @@ clause_reset_after_error()
 void
 timed_pause(double sleep_time)
 {
-#if defined(HAVE_USLEEP)
+#if defined(HAVE_USLEEP) && defined(USE_MOUSE)
     if (term->waitforinput)		/* If the terminal supports it */
 	while (sleep_time > 0.05) {	/* we poll 20 times a second */
 	    usleep(50000);		/* Sleep for 50 msec */
