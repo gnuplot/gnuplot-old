804b2dfd000de2f3a5035b0156af6638f737503a
sfeam <sfeam>
2014-03-03 04:09:30 +0000

    dead code removal

--- src/matrix.h
+++ src/matrix.h
@@ -1,5 +1,5 @@
 /*
- * $Id: matrix.h,v 1.12 2013/06/02 14:50:24 markisch Exp $
+ * $Id: matrix.h,v 1.13 2014/03/03 04:09:30 sfeam Exp $
  */
 
 /* GNUPLOT - matrix.h */
@@ -36,8 +36,7 @@ double  **matr __PROTO((int r, int c));
 void    free_matr __PROTO((double **m));
 double  *redim_vec __PROTO((double **v, int n));
 void    solve __PROTO((double **a, int n, double **b, int m));
-void    Givens __PROTO((double **C, double *d, double *x,
-                        double *r, int N, int n, int want_r));
+void    Givens __PROTO((double **C, double *d, double *x, int N, int n));
 void    Invert_RtR __PROTO((double **R, double **I, int n));
 
 /* Functions for use by THIN_PLATE_SPLINES_GRID method */
