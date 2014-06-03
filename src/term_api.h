47811ecc5042bc3cb2f26520adfa36b8f861ed2d
sfeam <sfeam>
2014-06-03 17:58:19 +0000

    Change implementation of greying out inactive wxt keybox entries to use a textbox rather than the active area of the key entry.

--- src/term_api.h
+++ src/term_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: term_api.h,v 1.132 2014/04/18 04:07:19 sfeam Exp $
+ * $Id: term_api.h,v 1.133 2014/06/03 17:58:20 sfeam Exp $
  */
 
 /* GNUPLOT - term_api.h */
@@ -205,7 +205,8 @@ typedef enum t_textbox_options {
 	TEXTBOX_OUTLINE,
 	TEXTBOX_BACKGROUNDFILL,
 	TEXTBOX_MARGINS,
-	TEXTBOX_FINISH
+	TEXTBOX_FINISH,
+	TEXTBOX_GREY
 } t_textbox_options;
 #endif
 
