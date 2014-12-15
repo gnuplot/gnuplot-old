9f9d0f835d560ee48e718c192503b70dc1cd8449
sfeam <sfeam>
2014-12-15 04:24:06 +0000

    Distinguish auto-generated replot events (e.g. replot-on-resize) from user-generated replot events ('e' hotkey or toolbar replot widget).

--- src/wxterminal/wxt_gui.cpp
+++ src/wxterminal/wxt_gui.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.cpp,v 1.128.2.8 2014/12/13 21:14:01 sfeam Exp $
+ * $Id: wxt_gui.cpp,v 1.128.2.9 2014/12/15 04:24:08 sfeam Exp $
  */
 
 /* GNUPLOT - wxt_gui.cpp */
@@ -730,8 +730,8 @@ void wxtFrame::OnSize( wxSizeEvent& event )
 #endif
 
 	/* Note: On some platforms OnSize() might get called before the settings have been initialized in wxt_init(). */
-	if ((wxt_redraw == yes) && replot_line && replot_line[0] != '\0')
-		wxt_exec_event(GE_keypress, 0, 0, 'e' , 0, this->GetId());
+	if (wxt_redraw == yes)
+		wxt_exec_event(GE_replot, 0, 0, 0 , 0, this->GetId());
 }
 
 /* wrapper for AddPendingEvent or ProcessEvent */
