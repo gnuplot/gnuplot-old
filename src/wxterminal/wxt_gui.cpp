4f7f033eec97537eab6b1fa09a0acf59e7757a91
sfeam <sfeam>
2014-12-30 19:45:28 +0000

    Use cairo_show_page() rather than cairo_surface_show_page()

--- src/wxterminal/wxt_gui.cpp
+++ src/wxterminal/wxt_gui.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.cpp,v 1.143 2014/12/26 23:21:48 sfeam Exp $
+ * $Id: wxt_gui.cpp,v 1.144 2014/12/30 19:45:29 sfeam Exp $
  */
 
 /* GNUPLOT - wxt_gui.cpp */
@@ -544,7 +544,7 @@ void wxtFrame::OnExport( wxCommandEvent& WXUNUSED( event ) )
 			1./(double)wxt_current_plot->oversampling_scale);
 		wxt_current_panel->wxt_cairo_refresh();
 
-		cairo_surface_show_page(surface);
+		cairo_show_page(wxt_current_plot->cr);
 		cairo_surface_finish(surface);
 		wxt_current_plot->cr = save_cr;
 		cairo_restore(wxt_current_plot->cr);
@@ -566,7 +566,7 @@ void wxtFrame::OnExport( wxCommandEvent& WXUNUSED( event ) )
 			1./(double)wxt_current_plot->oversampling_scale);
 		wxt_current_panel->wxt_cairo_refresh();
 
-		cairo_surface_show_page(surface);
+		cairo_show_page(wxt_current_plot->cr);
 		cairo_surface_finish(surface);
 		wxt_current_plot->cr = save_cr;
 		cairo_restore(wxt_current_plot->cr);
