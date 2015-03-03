e2beacba8a80bd88045c25508ef0b16556522353
sfeam <sfeam>
2015-03-03 06:17:03 +0000

    replace y1= and y2= with the single option y=

--- src/tables.c
+++ src/tables.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: tables.c,v 1.134 2014/09/14 18:21:13 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: tables.c,v 1.135 2015/03/03 06:17:04 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - tables.c */
@@ -714,6 +714,7 @@ const struct gen_table filledcurves_opts_tbl[] =
     { "r", FILLEDCURVES_ATR },
     { "above", FILLEDCURVES_ABOVE },
     { "below", FILLEDCURVES_BELOW },
+    { "y",  FILLEDCURVES_Y1 },
     { NULL, -1 }
 };
 
