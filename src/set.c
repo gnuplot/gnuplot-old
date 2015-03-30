9456e87409e4cffaa12735b26728a6abfc47ef96
sfeam <sfeam>
2015-03-30 15:34:15 +0000

    missing check for end-of-command

--- src/set.c
+++ src/set.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: set.c,v 1.459.2.13 2015/03/29 18:08:05 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: set.c,v 1.459.2.14 2015/03/30 15:34:16 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - set.c */
@@ -2802,7 +2802,8 @@ set_monochrome()
     struct lp_style_type mono_default[] = DEFAULT_MONO_LINETYPES;
 
     monochrome = TRUE;
-    c_token++;
+    if (!END_OF_COMMAND)
+	c_token++;
 
     if (almost_equals(c_token, "def$ault")) {
 	c_token++;
