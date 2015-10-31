f6231d7e5fea81b990630032975908ea57f9b34c
sfeam <sfeam>
2015-10-31 23:38:49 +0000

    Fix regression in 5.0.1 that left an extraneous '@' in title columnhead(N)

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.318 2015/10/01 03:38:43 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.319 2015/10/31 23:38:49 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -2829,6 +2829,8 @@ df_set_key_title(struct curve_points *plot)
 	    char *trailer = NULL;
 	    columnhead = strtol(placeholder+11, &trailer, 0);
 	    *placeholder = '\0';
+	    if (trailer && *trailer == '@')
+		trailer++;
 	    sprintf(newtitle, "%s%s%s", plot->title,
 		    (columnhead <= 0) ? df_key_title :
 		    (columnhead <= df_no_cols) ? df_column[columnhead-1].header : "",
