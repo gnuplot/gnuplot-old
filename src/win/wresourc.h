a81f5ad3202913741d5647cff52ff8162e137f0f
markisch <markisch>
2016-08-10 17:52:27 +0000

    Add a progress bar to print progress dialog.  Bump minimum Windows version to XP here, too.

--- src/win/wresourc.h
+++ src/win/wresourc.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wresourc.h,v 1.21 2016-08-10 17:39:30 markisch Exp $
+ * $Id: wresourc.h,v 1.22 2016-08-10 17:52:27 markisch Exp $
  */
 
 /* GNUPLOT - win/wresourc.h */
@@ -101,6 +101,7 @@
 #define PSIZE_OFFX 108
 #define PSIZE_OFFY 109
 #define CANCEL_PCDONE 120
+#define CANCEL_PROGRESS 121
 
 /* wgraph.c */
 /* line style dialog box */
