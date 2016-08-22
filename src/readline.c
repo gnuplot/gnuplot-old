198d0a6c0e827b675073b9e7627f13748b83d758
sfeam <sfeam>
2016-08-22 17:45:31 +0000

    clear errno before reading next character from input stream

--- src/readline.c
+++ src/readline.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: readline.c,v 1.74 2016-06-20 08:25:25 markisch Exp $"); }
+static char *RCSid() { return RCSid("$Id: readline.c,v 1.75 2016-08-22 17:45:31 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - readline.c */
@@ -81,6 +81,7 @@ getc_wrapper(FILE* fp)
 #if defined(WGP_CONSOLE)
 	    c = ConsoleGetch();
 #else
+	errno = 0;
 	if (fp)
 	    c = getc(fp);
 	else
