2b7fd6cffad3bafe69da0dac8aaab6693cc108e3
markisch <markisch>
2016-09-12 18:01:33 +0000

    Remove unused lower part of the 'general' print property sheet

--- src/win/wgraph.c
+++ src/win/wgraph.c
@@ -1,5 +1,5 @@
 /*
- * $Id: wgraph.c,v 1.222 2016-09-12 17:42:11 markisch Exp $
+ * $Id: wgraph.c,v 1.223 2016-09-12 18:01:34 markisch Exp $
  */
 
 /* GNUPLOT - win/wgraph.c */
@@ -3197,6 +3197,11 @@ CopyPrint(LPGW lpgw)
 	pd.lphPropertyPages = &hpsp;
 	pd.nStartPage = START_PAGE_GENERAL;
 	pd.lpCallback = PrintingCallbackCreate(&pr);
+
+	/* remove the lower part of the "general" property sheet */
+	pd.lpPrintTemplateName = TEXT("PrintDlgExEmpty");
+	pd.hInstance = graphwin->hInstance;
+	pd.Flags |= PD_ENABLEPRINTTEMPLATE;
 
 	if (PrintDlgEx(&pd) != S_OK) {
 		DWORD error = CommDlgExtendedError();
