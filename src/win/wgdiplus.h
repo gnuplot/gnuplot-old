2ceeefeb616f04eaf623cfad6c7f2fffad2fd053
markisch <markisch>
2016-11-18 10:04:40 +0000

    Determine initial font metrics etc. using the appropriate API.

--- src/win/wgdiplus.h
+++ src/win/wgdiplus.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wgdiplus.h,v 1.9 2016-11-05 10:01:10 markisch Exp $
+ * $Id: wgdiplus.h,v 1.10 2016-11-18 10:04:40 markisch Exp $
  */
 
 /*
@@ -47,6 +47,8 @@ extern void gdiplusSolidFilledPolygonEx(HDC hdc, POINT *ppt, int polyi, COLORREF
 extern void gdiplusPatternFilledPolygonEx(HDC hdc, POINT *ppt, int polyi, COLORREF color, double alpha, COLORREF backcolor, BOOL transparent, int style);
 
 extern void gdiplusCircleEx(HDC hdc, POINT *p, int radius, UINT style, float width, COLORREF color, double alpha);
+
+extern void InitFont_gdiplus(LPGW lpgw, HDC hdc, LPRECT rect);
 
 extern void drawgraph_gdiplus(LPGW lpgw, HDC hdc, LPRECT rect);
 extern void metafile_gdiplus(LPGW lpgw, HDC hdc, LPRECT rect, LPWSTR name);
