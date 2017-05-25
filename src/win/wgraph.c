50eea5faaca717c82f0b8710db3ca36c5de44e5f
markisch <markisch>
2017-05-25 18:22:10 +0000

    Let the gnuplot core know the current window ID

--- src/win/wgraph.c
+++ src/win/wgraph.c
@@ -1,5 +1,5 @@
 /*
- * $Id: wgraph.c,v 1.255 2017-05-21 08:43:44 markisch Exp $
+ * $Id: wgraph.c,v 1.255.2.1 2017-05-25 18:22:12 markisch Exp $
  */
 
 /* GNUPLOT - win/wgraph.c */
@@ -767,7 +767,7 @@ GraphEnd(LPGW lpgw)
 	lpgw->locked = FALSE;
 	UpdateWindow(lpgw->hWndGraph);
 #ifdef USE_MOUSE
-	gp_exec_event(GE_plotdone, 0, 0, 0, 0, 0);	/* notify main program */
+	gp_exec_event(GE_plotdone, 0, 0, 0, 0, lpgw->Id);	/* notify main program */
 #endif
 }
 
