198e01a06db94114f9993d8f9ba00b88261a4a4b
markisch <markisch>
2017-06-17 19:59:16 +0000

    Remove doublebuffer setting.

--- src/win/wgnuplib.h
+++ src/win/wgnuplib.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wgnuplib.h,v 1.89 2017-05-21 08:43:44 markisch Exp $
+ * $Id: wgnuplib.h,v 1.89.2.1 2017-06-17 19:59:18 markisch Exp $
  */
 
 /* GNUPLOT - win/wgnuplib.h */
@@ -343,7 +343,6 @@ typedef struct tagGW {
 	BOOL	color;		/* color pens? */
 	BOOL	dashed;		/* dashed lines? */
 	BOOL	rounded;	/* rounded line caps and joins? */
-	BOOL	doublebuffer;	/* double buffering? */
 	BOOL	oversample;	/* oversampling? */
 	BOOL	gdiplus;	/* Use GDI+ only backend? */
 	BOOL	d2d;
