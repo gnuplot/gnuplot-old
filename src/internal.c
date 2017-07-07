62cbc28257fbe1b91c6e8ea714f3a0e04f0af8ce
sfeam <sfeam>
2017-07-07 00:02:32 +0000

    gprintf_value() is like gprintf() except that it takes (struct value *) rather than (double)

--- src/internal.c
+++ src/internal.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: internal.c,v 1.100 2017-07-03 15:53:59 markisch Exp $"); }
+static char *RCSid() { return RCSid("$Id: internal.c,v 1.101 2017-07-07 00:02:33 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - internal.c */
@@ -1623,7 +1623,7 @@ f_gprintf(union argument *arg)
     buffer = gp_alloc(length, "f_gprintf");
 
     /* Call the old internal routine */
-    gprintf(buffer, length, fmt.v.string_val, base, real(&val));
+    gprintf_value(buffer, length, fmt.v.string_val, base, &val);
 
     FPRINTF((stderr," gprintf result = \"%s\"\n",buffer));
     push(Gstring(&result, buffer));
