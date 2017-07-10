7b1b66f8ed810ebe86420105d9ea62b96293522d
markisch <markisch>
2017-07-10 10:09:07 +0000

    SaveAsBitmap: Get a bitmap of the screen output with the help of a new routine GraphGetBitmap() extracted from CopyClip().

--- src/win/wcommon.h
+++ src/win/wcommon.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wcommon.h,v 1.32 2017-06-24 09:15:38 markisch Exp $
+ * $Id: wcommon.h,v 1.33 2017-07-10 10:09:07 markisch Exp $
  */
 
 /* GNUPLOT - wcommon.h */
@@ -120,6 +120,7 @@ void clear_tooltips(LPGW lpgw);
 void draw_update_keybox(LPGW lpgw, unsigned plotno, unsigned x, unsigned y);
 int draw_enhanced_text(LPGW lpgw, LPRECT rect, int x, int y, const char * str);
 void draw_get_enhanced_text_extend(PRECT extend);
+HBITMAP GraphGetBitmap(LPGW lpgw);
 
 #ifdef __cplusplus
 }
