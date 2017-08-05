1768127853f7a3bd9849f0ea05a26b6a67dd4f95
markisch <markisch>
2017-08-05 20:04:55 +0000

    Scale pattern with resolution also for D2D 1.0

--- src/win/wd2d.cpp
+++ src/win/wd2d.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wd2d.cpp,v 1.32 2017-07-31 19:36:05 markisch Exp $
+ * $Id: wd2d.cpp,v 1.33 2017-08-05 20:04:56 markisch Exp $
  */
 
 /*
@@ -619,7 +619,11 @@ d2dCreatePatternBrush(LPGW lpgw, unsigned pattern, COLORREF color, bool transpar
 
 	// scale dash pattern with resolution
 	FLOAT dpiX, dpiY;
+#ifdef HAVE_D2D11
 	pMainRenderTarget->GetDpi(&dpiX, &dpiY);
+#else
+	g_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+#endif
 	FLOAT scale = dpiX / 96.f;
 	FLOAT size = 96.f * scale;
 
